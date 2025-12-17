7ae9223cf07ad4a91de13f0e96d00ec92d24cbdb
yeeunkim7 <tyeole7172@naver.com>
2025-12-17 20:45:18 +0900

    chore: initialize project package structure

---
+++ src/main/java/com/example/accounting/AccountingApplication.java
@@ -0,0 +1,8 @@
+package com.example.accounting;
+
+@SpringBootApplication
+public class AccountingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingApplication.class, args);
+    }
+}
